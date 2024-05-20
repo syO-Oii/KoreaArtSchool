4b3aa11eb36b75513003162597d50da862d2bd53
leeje <155074293+leejesjane@users.noreply.github.com>
2024-05-20 09:35:11 +0900

    Update CommCd.java

--- src/main/java/com/maximum/koreaArtSchool/entity/CommCd.java
+++ src/main/java/com/maximum/koreaArtSchool/entity/CommCd.java
@@ -6,31 +6,32 @@ import lombok.Data;
 import java.sql.Timestamp;
 
 @Data
+@Table(name = "COMM_CD")
 @Entity
 public class CommCd {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cd_id")
+    @Column(name = "CD_ID")
     private int cdId;
 
-    @Column(name = "cd_se", nullable = false, length = 10)
+    @Column(name = "CD_SE", nullable = false, length = 10)
     private String cdSe;
 
-    @Column(name = "cd", nullable = false, length = 10)
+    @Column(name = "CD", nullable = false, length = 10)
     private String cd;
 
-    @Column(name = "cd_nm", nullable = false, length = 20)
+    @Column(name = "CD_NM", nullable = false, length = 20)
     private String cdNm;
 
-    @Column(name = "cd_desc", length = 100)
+    @Column(name = "CD_DESC", length = 100)
     private String cdDesc;
 
-    @Column(name = "is_yn", nullable = false, length = 2)
+    @Column(name = "IS_YN", nullable = false, length = 2)
     private String isYn;
 
-    @Column(name = "created_at", nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Column(name = "CREATED_AT", nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Timestamp createdAt;
 
-    @Column(name = "deleted_at")
+    @Column(name = "DELETED_AT")
     private Timestamp deletedAt;
 }
