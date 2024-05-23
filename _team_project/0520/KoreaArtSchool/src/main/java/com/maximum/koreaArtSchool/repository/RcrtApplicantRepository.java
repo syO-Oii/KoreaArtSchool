f57fa81535d2229999a65c201078480c99f04140
leeje <155074293+leejesjane@users.noreply.github.com>
2024-05-23 11:13:43 +0900

    Delete directory

--- _team_project/0520/KoreaArtSchool/src/main/java/com/maximum/koreaArtSchool/repository/RcrtApplicantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maximum.koreaArtSchool.repository;
-
-import com.maximum.koreaArtSchool.entity.RcrtApplicant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RcrtApplicantRepository extends JpaRepository<RcrtApplicant, Integer> {
-}
