83c0d236d7de433de3425e54209b9eb7c2bb5f87
leeje <155074293+leejesjane@users.noreply.github.com>
2024-05-24 05:01:45 +0900

    Update KoreaArtSchoolApplication
    Update KoreaArtSchoolApplication

--- src/main/java/com/maximum/koreaArtSchool/KoreaArtSchoolApplication.java
+++ src/main/java/com/maximum/koreaArtSchool/KoreaArtSchoolApplication.java
@@ -3,9 +3,11 @@ package com.maximum.koreaArtSchool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCaching
+@ComponentScan
 public class KoreaArtSchoolApplication {
 
 	public static void main(String[] args) {
