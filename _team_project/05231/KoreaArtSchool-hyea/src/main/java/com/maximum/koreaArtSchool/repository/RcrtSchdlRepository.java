c4b2a543f8edf55f9998913097a20f69798a4069
leeje <155074293+leejesjane@users.noreply.github.com>
2024-05-23 09:51:34 +0900

    Delete directory

--- _team_project/05231/KoreaArtSchool-hyea/src/main/java/com/maximum/koreaArtSchool/repository/RcrtSchdlRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maximum.koreaArtSchool.repository;
-
-import com.maximum.koreaArtSchool.entity.RcrtSchdl;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RcrtSchdlRepository extends JpaRepository<RcrtSchdl, Integer> {
-}
