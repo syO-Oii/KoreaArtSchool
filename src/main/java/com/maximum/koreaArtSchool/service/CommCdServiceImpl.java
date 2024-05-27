37ca27a8adc4535f367dd7cc388c4795b900fb98
leeje <155074293+leejesjane@users.noreply.github.com>
2024-05-27 21:03:05 +0900

    Update

--- src/main/java/com/maximum/koreaArtSchool/service/CommCdServiceImpl.java
+++ src/main/java/com/maximum/koreaArtSchool/service/CommCdServiceImpl.java
@@ -1,118 +1,90 @@
 package com.maximum.koreaArtSchool.service;
 
-import com.maximum.koreaArtSchool.dto.CommonCodeDto;
-import com.maximum.koreaArtSchool.dto.ConvertToDto;
-import com.maximum.koreaArtSchool.entity.CommCd;
-import com.maximum.koreaArtSchool.repository.CommCdRepository;
-import lombok.RequiredArgsConstructor;
+import com.maximum.koreaArtSchool.dto.SearchParamCommonCode;
+import com.maximum.koreaArtSchool.dto.UpdateCommonCode;
+import com.maximum.koreaArtSchool.entity.CommonCode;
+import com.maximum.koreaArtSchool.repository.CommonCodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
-@RequiredArgsConstructor
 @Service
-public class CommCdServiceImpl implements CommCdService {
+public class CommonCodeServiceImpl implements CommonCodeService {
 
     @Autowired
-    private final CommCdRepository commCdRepository;
-    private final ConvertToDto converter = new ConvertToDto();
+    private CommonCodeRepository commonCodeRepository;
+    
+    @Override
+    public List<String> findAllDetailCode() {
+        List<String> detailCodes = commonCodeRepository.findAllDetailCode();
+        if (detailCodes.isEmpty()) {
+            System.out.println("No detail codes found");
+        } else {
+            detailCodes.forEach(code -> System.out.println("Found detail code: " + code));
+        }
+        return detailCodes;
+    }
 
     @Override
-    public List<CommCd> searchByCdSeAndCdNm(String cdSe, String cdNm) {
-        if (cdSe != null && cdNm != null) {
-            return commCdRepository.findByCdSeAndCdNm(cdSe, cdNm);
-        } else if (cdSe != null) {
-            return commCdRepository.findByCdSe(cdSe);
-        } else if (cdNm != null) {
-            return commCdRepository.findByCdNm(cdNm);
+    @Transactional
+    public CommonCode saveCommonCode(CommonCode commCd) {
+        return commonCodeRepository.save(commCd);
+    }
+
+    @Override
+    @Transactional
+    public CommonCode updateCommonCode(Integer commonCodeNo, UpdateCommonCode update) {
+        Optional<CommonCode> optionalCommonCode = commonCodeRepository.findById(commonCodeNo);
+        if (optionalCommonCode.isPresent()) {
+            CommonCode commCd = optionalCommonCode.get();
+            commCd.setCdSe(update.getCdSe());
+            commCd.setCd(update.getCd());
+            commCd.setCdNm(update.getCdNm());
+            commCd.setCdDesc(update.getCdDesc());
+            commCd.setIsActiveYn(update.getIsActiveYn());
+            return commonCodeRepository.save(commCd);
         } else {
-            return commCdRepository.findAll();
+            throw new RuntimeException("CommonCode not found with id" + commonCodeNo);
         }
     }
 
     @Override
-    public CommonCodeDto getCommonCodeById(Integer cdId) {
-        CommCd commCd = commCdRepository.findById(cdId).orElse(null);
-        if (commCd != null) {
-            return converter.convertToDto(commCd);
-        } else {
-            return null;
+    @Transactional
+    public void deleteCommonCode(Integer commonCodeNo) {
+        commonCodeRepository.deleteById(commonCodeNo);
+    }
+
+    @Override
+    public Page<CommonCode> findAll(Pageable pageable) {
+        return commonCodeRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<CommonCode> getCommonCodes(SearchParamCommonCode searchParam, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return search(searchParam, pageable);
+    }
+
+    private Page<CommonCode> search(SearchParamCommonCode searchParam, Pageable pageable) {
+        Specification<CommonCode> spec = Specification.where(null);
+
+        if (searchParam.getCdSe() != null && !searchParam.getCdSe().isEmpty() && !"전체".equals(searchParam.getCdSe())) {
+            spec = spec.and((root, query, cb) -> cb.equal(root.get("cdSe"), searchParam.getCdSe()));
         }
-    }
+        if (searchParam.getCd() != null && !searchParam.getCd().isEmpty() && !"전체".equals(searchParam.getCd())) {
+            spec = spec.and((root, query, cb) -> cb.equal(root.get("cd"), searchParam.getCd()));
+        }
+        if (searchParam.getCdNm() != null && !searchParam.getCdNm().isEmpty() && !"전체".equals(searchParam.getCdNm())) {
+            spec = spec.and((root, query, cb) -> cb.like(root.get("cdNm"), "%" + searchParam.getCdNm() + "%"));
+        }
 
-    @Override
-    public Page<CommCd> getAllCommCd(int page, int size) {
-        Pageable pageable = PageRequest.of(page, size, Sort.by("CdId").descending());
-        return commCdRepository.findAll(pageable);
-    }
-
-    @Override
-    public List<String> getAllCdNmList() {
-        List<CommCd> allCommCds = commCdRepository.findAll();
-        return allCommCds.stream()
-                .map(CommCd::getCdNm)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public void addCode(String cd, String cdSe, String cdNm, String cdDesc, String isYn) {
-        CommCd newCode = new CommCd();
-        newCode.setCd(cd);
-        newCode.setCdSe(cdSe);
-        newCode.setCdNm(cdNm);
-        newCode.setCdDesc(cdDesc);
-        newCode.setIsYn(isYn);
-        commCdRepository.save(newCode);
-    }
-
-    @Override
-    public CommCd getCommCdById(Integer cdId) {
-        return commCdRepository.findById(cdId).orElse(null);
-    }
-
-    @Cacheable("commCds")
-    @Override
-    public List<CommCd> getAllCommCds() {
-        return commCdRepository.findAll();
-    }
-
-    @Cacheable(value = "commCdsByCdSe", key = "#cdSe")
-    @Override
-    public List<CommCd> getCommCdsByCdSe(String cdSe) {
-        return commCdRepository.findByCdSe(cdSe);
-    }
-
-    @Cacheable(value = "commCd", key = "#cdSe + '-' + #cd")
-    @Override
-    public String getCdNmByCdSeAndCd(String cdSe, String cd) {
-        return commCdRepository.findByCdSeAndCd(cdSe, cd).map(CommCd::getCdNm).orElse(null);
-    }
-
-    @Transactional
-    @Override
-    public CommCd updateCommCd(Integer cdId, String cd, String cdSe, String cdNm, String cdDesc, String isYn) {
-        CommCd existingCommCd = commCdRepository.findById(cdId)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid CommCd ID: " + cdId));
-        existingCommCd.setCd(cd);
-        existingCommCd.setCdSe(cdSe);
-        existingCommCd.setCdNm(cdNm);
-        existingCommCd.setCdDesc(cdDesc);
-        existingCommCd.setIsYn(isYn);
-        return commCdRepository.save(existingCommCd);
-    }
-
-    @Transactional
-    @Override
-    public void deleteCommCd(Integer cdId) {
-        commCdRepository.deleteById(cdId);
+        return commonCodeRepository.findAll(spec, pageable);
     }
 }
-
