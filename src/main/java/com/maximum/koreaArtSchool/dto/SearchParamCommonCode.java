4deca8cb8d9078d9eaeed86de55a7ef21009434e
leeje <155074293+leejesjane@users.noreply.github.com>
2024-05-27 20:59:42 +0900

    Update
    Update

---
+++ src/main/java/com/maximum/koreaArtSchool/dto/SearchParamCommonCode.java
@@ -0,0 +1,10 @@
+package com.maximum.koreaArtSchool.dto;
+
+import lombok.Data;
+
+@Data
+public class SearchParamCommonCode {
+    private String cdSe;
+    private String cd;
+    private String cdNm;
+}
